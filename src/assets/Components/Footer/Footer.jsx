bff6a0844442c2e973a3782b0bac9ffeccf06248
sudiptodas153 <sudiptodas4093@gmail.com>
2025-04-24 00:53:09 +0600

    add social media icon with profile link

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import image from '../../images/logo.png'
 import { NavLink } from 'react-router';
-import { FaFacebook, FaGithub, FaInstagramSquare, FaYoutube } from 'react-icons/fa';
+import { FaFacebook, FaGithub, FaInstagramSquare, FaLinkedin, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
     const links = <>
@@ -24,10 +24,10 @@ const Footer = () => {
             </div>
 
             <div className='flex items-center justify-center gap-5'>
-                <a href=''><FaFacebook size={30} /></a>
-                <a href=''><FaGithub size={30}/></a>
-                <a href=''><FaInstagramSquare size={30}/></a>
-                <a href=''><FaYoutube size={35}/></a>
+                <a target='blank' className='text-blue-500' href='https://www.facebook.com/profile.php?id=100094053365815'><FaFacebook size={30} /></a>
+                <a target='blank' href='https://github.com/sudiptodas153'><FaGithub size={30}/></a>
+                <a target='blank' className='text-blue-500'  href='https://www.linkedin.com/in/sudipto-das-677330343/'><FaLinkedin size={30}/></a>
+                <a target='blank' className='text-red-700 ' href='https://www.youtube.com/@As-Sudipto'><FaYoutube size={35}/></a>
             </div>
             
         </div>
