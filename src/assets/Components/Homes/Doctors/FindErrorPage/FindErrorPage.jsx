9c21be98e5bef99ee62ab53969c98e819ef9e73f
sudiptodas153 <sudiptodas4093@gmail.com>
2025-04-24 18:01:43 +0600

    remove some feature

--- src/assets/Components/Homes/Doctors/FindErrorPage/FindErrorPage.jsx
+++ src/assets/Components/Homes/Doctors/FindErrorPage/FindErrorPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router';
+import { Link, useParams } from 'react-router';
 
 const FindErrorPage = () => {
-    const errorId = useLoaderData()
-    console.log(errorId)
+    const {id} = useParams();
     return (
         <div className='bg-white text-center mt-10 px-3 space-y-3 py-10 mx-3 rounded-2xl'>
            <h2 className='text-3xl font-bold'>No Doctor Found!!</h2>
            <p className='text-gray-500'>No Doctor Found with this registration No-</p>
+           <p className='text-gray-500 text-sm font-bold'>No: {id}</p>
           <Link to={'/'}> <button className='btn bg-blue-600 text-white'>View All Doctors</button></Link>
         </div>
     );
